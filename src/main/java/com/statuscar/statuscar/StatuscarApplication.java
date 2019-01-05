e0e8c725681559b10432ac1688ace8eca2db22d7
Alberto Costa <mihcadevs@gmail.com>
2019-01-05 18:21:27 -0200

    Projeto Criado

---
+++ src/main/java/com/statuscar/statuscar/StatuscarApplication.java
@@ -0,0 +1,14 @@
+package com.statuscar.statuscar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatuscarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatuscarApplication.class, args);
+	}
+
+}
+
